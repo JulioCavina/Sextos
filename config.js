7f767299aabe990502f196d7b69f4d11e9d30bdb
JulioCavina <74439658+JulioCavina@users.noreply.github.com>
2026-05-31 21:23:32 -0300

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // 1) Cole aqui a URL /exec do Apps Script depois de implantar o Web App.
 // 2) Mantenha a lista de palavras neste caminho no GitHub Pages.
 window.SEXTO_CONFIG = {
-  API_URL: 'COLE_AQUI_A_URL_DO_APPS_SCRIPT',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzjeNncJ96A5JXWfBY-Ey28xkCkDrPe5fI__vDbnkD8HAw6jgkSJ1zpbcxbN9iDTLN1/exec',
   WORD_LIST_URL: 'palavras/palavras_sexto_6_letras_filtradas_curadas.txt',
   APP_VERSION: '1.0.0'
 };
